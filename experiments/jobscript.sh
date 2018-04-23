e4c0f34f0020cb1773dcdf411da76f1390687162
Henrik Barthels <barthels@aices.rwth-aachen.de>
2018-04-23 14:19:33 +0200

    Changed jobscript

--- experiments/jobscript.sh
+++ experiments/jobscript.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-#BSUB -J "linnea_gen"    # job name
-#BSUB -W 0:15            # limits in hours:minutes
+#BSUB -J "linnea_gen"       # job name
+#BSUB -o linnea_gen.%J.cout # job output 
+#BSUB -W 0:15               # limits in hours:minutes
 #BSUB -M 10000 # memory in MB
 
 module load python/3.6.0
