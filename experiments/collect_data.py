ad260785dae546fd1286386d98970d3e269cb232
Henrik Barthels <barthels@aices.rwth-aachen.de>
2018-04-23 16:41:22 +0200

    Added collect_data.py

---
+++ experiments/collect_data.py
@@ -0,0 +1,11 @@
+import os.path
+import pandas as pd
+
+if __name__ == '__main__':
+
+    data = []
+    for jobindex in range(1, 31):
+        data.append(pd.read_pickle("linnea_generation{}.pkl".format(job_index)))
+
+    result = pd.concat(data)
+    result.to_csv("linnea_generation.csv")
