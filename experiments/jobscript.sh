0cbd0a27365685d4bc3147a41b2f3b392bd25345
Henrik Barthels <barthels@aices.rwth-aachen.de>
2018-04-23 17:41:21 +0200

    Changed jobscript.

--- experiments/jobscript.sh
+++ experiments/jobscript.sh
@@ -9,15 +9,15 @@ module load python/3.6.0
 
 # NEW_INDEX=$((JOB_INDEX-1))
 # echo $NEW_INDEX
-DATE=$(date +'%Y_%m_%d_%H_%M_%S')
+# DATE=$(date +'%Y_%m_%d_%H_%M_%S')
 
 # SEED=${LSB_JOBINDEX}
 
 cd /home/hb765588/linnea
 source linnea_venv/bin/activate
 cd /home/hb765588/linnea/results/generation
-mkdir -p run_${DATE}
-cd run_${DATE}
+mkdir -p run_%J
+cd run_%J
 # cp ../config.json .
 python3 /home/hb765588/linnea/linnea/experiments/experiments_test.py ${LSB_JOBINDEX} 1
 
