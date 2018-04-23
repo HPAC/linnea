afa29296c068732d34a715956befc871943eaf7e
Henrik Barthels <barthels@aices.rwth-aachen.de>
2018-04-23 17:51:18 +0200

    Fixed bug.

--- experiments/jobscript.sh
+++ experiments/jobscript.sh
@@ -16,8 +16,8 @@ module load python/3.6.0
 cd /home/hb765588/linnea
 source linnea_venv/bin/activate
 cd /home/hb765588/linnea/results/generation
-mkdir -p run_%J
-cd run_%J
+mkdir -p run_${LSB_JOBID}
+cd run_${LSB_JOBID}
 # cp ../config.json .
 python3 /home/hb765588/linnea/linnea/experiments/experiments_test.py ${LSB_JOBINDEX} 1
 
