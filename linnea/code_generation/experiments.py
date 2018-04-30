4842412e0168bb111cb632b580158dca42f1b1c1
Henrik Barthels <barthels@aices.rwth-aachen.de>
2018-04-30 14:55:47 +0200

    Changed some generated code.

--- linnea/code_generation/experiments.py
+++ linnea/code_generation/experiments.py
@@ -113,25 +113,23 @@ benchmarker_code = {config.Language.Julia: textwrap.dedent(
                     ),
                     config.Language.Matlab: textwrap.dedent(
                     """
-                    function runner()
-                        import MatrixGenerator.*;
-                        % hardcoded directory with naive and recommended implementation
-                        algorithms_dir = fullfile(fileparts(mfilename('fullpath')), 'algorithms');
-                        addpath(algorithms_dir);
-                        matrices = operand_generator();
-                        naive_ = @() naive(matrices{{:}});
-                        recommended_ = @() recommended(matrices{{:}});
-                        naive_mat = naive_();
-                        recomd_mat = recommended_();
-                        %fprintf('Norm(naive - recomd) %f\\n', norm(naive_mat - recomd_mat));
-                        %fprintf('Naive(0, 0): %f\\n', naive_mat(1, 1));
-                        %fprintf('Naive(0, 0): %f\\n', recomd_mat(1, 1));
-                    
-                        benchmarker = Benchmarker();
-                        benchmarker.benchmark('naive_matlab', 10, naive_);
-                        benchmarker.benchmark('recommended_matlab', 10, recommended_);
-                        benchmarker.save('matlab_results_{0}.txt');
-                    end
+                    import MatrixGenerator.*;
+                    % hardcoded directory with naive and recommended implementation
+                    algorithms_dir = fullfile('algorithms');
+                    addpath(algorithms_dir);
+                    matrices = operand_generator();
+                    naive_ = @() naive(matrices{{:}});
+                    recommended_ = @() recommended(matrices{{:}});
+                    naive_mat = naive_();
+                    recomd_mat = recommended_();
+                    %fprintf('Norm(naive - recomd) %f\\n', norm(naive_mat - recomd_mat));
+                    %fprintf('Naive(0, 0): %f\\n', naive_mat(1, 1));
+                    %fprintf('Naive(0, 0): %f\\n', recomd_mat(1, 1));
+                
+                    benchmarker = Benchmarker();
+                    benchmarker.benchmark('naive_matlab', 10, naive_);
+                    benchmarker.benchmark('recommended_matlab', 10, recommended_);
+                    benchmarker.save('matlab_results_{0}.txt');
                     """
                     ),
                     config.Language.Cpp: textwrap.dedent(
