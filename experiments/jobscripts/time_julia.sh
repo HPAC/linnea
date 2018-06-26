ffc8faaa1b2b5cf4b6f4323116466355e659f692
Henrik Barthels <barthels@aices.rwth-aachen.de>
2018-06-26 16:27:55 +0200

    Fixed bug.

--- experiments/jobscripts/time_julia.sh
+++ experiments/jobscripts/time_julia.sh
@@ -14,11 +14,11 @@ cd ${HOME}/linnea/results/execution
 mkdir -p run_${LSB_JOBID}
 cd run_${LSB_JOBID}
 
-if [ -f ${HOME}/linnea/output/lamp_example${LSB_JOBID}/Julia/runner.jl ]; then
-    # echo "File found!: ${HOME}/linnea/output/lamp_example${LSB_JOBID}/Julia/runner.jl"
-    ${HOME}/julia/julia-0.6.3/julia ${HOME}/linnea/output/lamp_example${LSB_JOBID}/Julia/runner.jl > julia_results_lamp_example${LSB_JOBID}.txt 2>&1
+if [ -f ${HOME}/linnea/output/lamp_example${LSB_JOBINDEX}/Julia/runner.jl ]; then
+    # echo "File found!: ${HOME}/linnea/output/lamp_example${LSB_JOBINDEX}/Julia/runner.jl"
+    ${HOME}/julia/julia-0.6.3/julia ${HOME}/linnea/output/lamp_example${LSB_JOBINDEX}/Julia/runner.jl > julia_results_lamp_example${LSB_JOBINDEX}.txt 2>&1
 else
-    echo "File not found: ${HOME}/linnea/output/lamp_example${LSB_JOBID}/Julia/runner.jl"
+    echo "File not found: ${HOME}/linnea/output/lamp_example${LSB_JOBINDEX}/Julia/runner.jl"
 fi
 
 exit
