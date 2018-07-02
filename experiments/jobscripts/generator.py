53633bc9f01aaf69d5a2e700397b7fdd6f4ac2ee
Henrik Barthels <barthels@aices.rwth-aachen.de>
2018-07-02 15:22:24 +0200

    Added generator for jobscripts.

---
+++ experiments/jobscripts/generator.py
@@ -0,0 +1,58 @@
+import pkg_resources
+import json
+import os.path
+import itertools
+
+# this causes a problem because it tries to read the config.json in the current directory.
+# from linnea.config import Strategy
+
+def main():
+
+    replacement = dict()
+    
+    config_file = "config.json"
+    if os.path.exists(config_file):
+        with open(config_file) as jsonfile:
+            for key, value in json.load(jsonfile).items():
+                if key == "exclusive":
+                    if value:
+                        replacement["exclusive"] = "#BSUB -x                     # exclusive access"
+                    else:
+                        replacement["exclusive"] = ""
+                else:
+                    replacement[key] = value    
+
+    template_path = "templates/time_generation.sh"
+    template_str = pkg_resources.resource_string(__name__, template_path).decode("UTF-8")
+
+    file_name = "time_generation_test.sh"
+    for strategy, merging in itertools.product(["c", "e"], [True, False]):
+        replacement_copy = replacement.copy()
+
+        file_name_parts = ["time_gen"]
+
+        file_name_parts.append(strategy)
+        replacement_copy["strategy"] = strategy
+        # if strategy is Strategy.constructive:
+        #     file_name_parts.append("c")
+        #     replacement_copy["strategy"] = "c"
+        # elif strategy is Strategy.exhaustive:
+        #     file_name_parts.append("e")
+        #     replacement_copy["strategy"] = "e"
+
+        if merging:
+            file_name_parts.append("m")
+            replacement_copy["merging"] = "true"
+        else:
+            file_name_parts.append("nm")
+            replacement_copy["merging"] = "false"
+
+        print(replacement_copy)
+        file_name_parts.append(".sh")
+        file_name = "_".join(file_name_parts)
+        with open(file_name, "wt", encoding='utf-8') as output_file:
+            output_file.write(template_str.format(**replacement_copy))
+
+
+if __name__ == '__main__':
+    main()
---
+++ experiments/jobscripts/templates/time_generation.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#BSUB -J "linnea_time[1-31]" # job name
+#BSUB -o "linnea/results/generation/run_%J/cout.txt" # job output
+#BSUB -W {time}:00            # limits in hours:minutes
+#BSUB -M {memory}            # memory in MB
+#BSUB -P {group}
+#BSUB -R model=={model}
+{exclusive}
+
+module load python/3.6.0
+
+cd ${{HOME}}/linnea
+source linnea_venv/bin/activate
+cd ${{HOME}}/linnea/results/generation
+mkdir -p run_${{LSB_JOBID}}
+cd run_${{LSB_JOBID}}
+python3 ${{HOME}}/linnea/linnea/experiments/experiments.py time_generation -j=${{LSB_JOBINDEX}} -r={repetitions} -{strategy} -m={merging}
+
+exit
