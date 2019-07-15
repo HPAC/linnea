2592fdec745c5fb1fba89c34c88a1b37f1e17767
Henrik Barthels <barthels@aices.rwth-aachen.de>
2019-07-15 21:04:27 +0200

    Using main() function.

--- linnea/code_generation/experiments/templates/Julia/runner.jl
+++ linnea/code_generation/experiments/templates/Julia/runner.jl
@@ -10,21 +10,25 @@ include("operand_generator.jl")
 include("reference/naive.jl")
 include("reference/recommended.jl")
 
-matrices = operand_generator()
+function main()
+    matrices = operand_generator()
 
-@info("Performing Test run...")
-result_naive = collect(naive(map(copy, matrices)...)[1])
-result_recommended = collect(recommended(map(copy, matrices)...)[1])
-{tmp_results}
-@test isapprox(result_recommended, result_naive, rtol=1e-3)
-{tests}
-@info("Test run performed successfully")
+    @info("Performing Test run...")
+    result_naive = collect(naive(map(copy, matrices)...)[1])
+    result_recommended = collect(recommended(map(copy, matrices)...)[1])
+    {tmp_results}
+    @test isapprox(result_recommended, result_naive, rtol=1e-3)
+    {tests}
+    @info("Test run performed successfully")
 
 
-@info("Running Benchmarks...")
-plotter = Benchmarker.Plot("julia_results_{experiment_name}", ["algorithm"; "threads"]);
-{measurements}
-Benchmarker.add_data(plotter, ["naive_julia"; {num_threads}], Benchmarker.measure(20, naive, matrices...) );
-Benchmarker.add_data(plotter, ["recommended_julia"; {num_threads}], Benchmarker.measure(20, recommended, matrices...) );
-Benchmarker.finish(plotter);
-@info("Benchmarks complete")
+    @info("Running Benchmarks...")
+    plotter = Benchmarker.Plot("julia_results_{experiment_name}", ["algorithm"; "threads"]);
+    {measurements}
+    Benchmarker.add_data(plotter, ["naive_julia"; {num_threads}], Benchmarker.measure(20, naive, matrices...) );
+    Benchmarker.add_data(plotter, ["recommended_julia"; {num_threads}], Benchmarker.measure(20, recommended, matrices...) );
+    Benchmarker.finish(plotter);
+    @info("Benchmarks complete")
+end
+
+main()
