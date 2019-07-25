be4c7236326677c4ac514e3fe289b9050e69623c
Henrik Barthels <barthels@aices.rwth-aachen.de>
2019-07-25 15:10:16 +0200

    Garbage collection is not disabled anymore.

--- linnea/code_generation/experiments/templates/Julia/algorithm_experiments.jl
+++ linnea/code_generation/experiments/templates/Julia/algorithm_experiments.jl
@@ -5,12 +5,10 @@ function {}({})
     start::Float64 = 0.0
     finish::Float64 = 0.0
     Benchmarker.cachescrub()
-    GC.enable(false)
     start = time_ns()
 
 {}
 
     finish = time_ns()
-    GC.enable(true)
     return (tuple({}), (finish-start)*1e-9)
 end
