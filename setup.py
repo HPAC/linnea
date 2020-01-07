9e6ae938f93b8c60adec46fcc2d8cdcbd01fc565
Henrik Barthels <barthels@aices.rwth-aachen.de>
2020-01-07 15:37:29 +0100

    Added MANIFEST.in.

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(name="linnea",
     author_email="barthels@aices.rwth-aachen.de",
     license="GPLv3",
     packages=find_packages(exclude=('tests', )),
+    include_package_data=True,
     zip_safe=True,
     python_requires=">=3.6",
     install_requires=[
